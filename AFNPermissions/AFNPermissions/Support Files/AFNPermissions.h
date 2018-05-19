5880b1b7414c319def68481e01d4a9ec1c9be919
David Messing <david.messing@gmail.com>
2018-05-19 11:48:04 -0500

    Initial project

---
+++ AFNPermissions/AFNPermissions/Support Files/AFNPermissions.h
@@ -0,0 +1,19 @@
+//
+//  AFNPermissions.h
+//  AFNPermissions
+//
+//  Created by David Messing on 5/19/18.
+//  Copyright © 2018 davemess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFNPermissions.
+FOUNDATION_EXPORT double AFNPermissionsVersionNumber;
+
+//! Project version string for AFNPermissions.
+FOUNDATION_EXPORT const unsigned char AFNPermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNPermissions/PublicHeader.h>
+
+
